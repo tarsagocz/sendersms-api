560e32067142ef9d3f57e40976f81b9a67aa4524
Bc. Marek Fajfr <fajfrmar@fit.cvut.cz>
2019-11-11 10:42:53 +0100

    Sending messages

---
+++ src/Models/Subscriber/SubscriberCreatedByEnumeration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Bc. Marek Fajfr <mfajfr90(at)gmail.com>
+ * Created at: 10:41 11.11.2019
+ */
+
+namespace Smswizz\Models\Subscriber;
+
+class SubscriberCreatedByEnumeration
+{
+}
